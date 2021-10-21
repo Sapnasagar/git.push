8297b80e68783d06ae4d74064461f5f457593978
Sapnasagar <54404347+Sapnasagar@users.noreply.github.com>
2021-10-21 13:43:08 +0530

    Add files via upload

---
+++ brewery.api - Copy/brewery1.js
@@ -0,0 +1,452 @@
+
+
+const breweries = [
+    {
+        "id": "bnaf-llc-austin",
+        "name": "Bnaf, LLC",
+        "brewery_type": "planning",
+        "street": null,
+        "address_2": null,
+        "address_3": null,
+        "city": "Austin",
+        "state": "Texas",
+        "county_province": null,
+        "postal_code": "78727-7602",
+        "country": "United States",
+        "longitude": null,
+        "latitude": null,
+        "phone": null,
+        "website_url": null,
+        "updated_at": "2018-07-24T00:00:00.000Z",
+        "created_at": "2018-07-24T00:00:00.000Z"
+      },
+      {
+        "id": "boulder-beer-co-boulder",
+        "name": "Boulder Beer Co",
+        "brewery_type": "regional",
+        "street": "2880 Wilderness Pl",
+        "address_2": null,
+        "address_3": null,
+        "city": "Boulder",
+        "state": "Colorado",
+        "county_province": null,
+        "postal_code": "80301-5401",
+        "country": "United States",
+        "longitude": "-105.2480158",
+        "latitude": "40.026439",
+        "phone": null,
+        "website_url": null,
+        "updated_at": "2018-08-24T00:00:00.000Z",
+        "created_at": "2018-07-24T00:00:00.000Z"
+      },
+      {
+        "id": "clermont-brewing-company-clermont",
+        "name": "Clermont Brewing Company",
+        "brewery_type": "planning",
+        "street": null,
+        "address_2": null,
+        "address_3": null,
+        "city": "Clermont",
+        "state": "Florida",
+        "county_province": null,
+        "postal_code": "34711-2108",
+        "country": "United States",
+        "longitude": null,
+        "latitude": null,
+        "phone": null,
+        "website_url": null,
+        "updated_at": "2018-08-11T00:00:00.000Z",
+        "created_at": "2018-07-24T00:00:00.000Z"
+      },
+      {
+        "id": "dimensional-brewing-co-dubuque",
+        "name": "Dimensional Brewing Co.",
+        "brewery_type": "planning",
+        "street": null,
+        "address_2": null,
+        "address_3": null,
+        "city": "Dubuque",
+        "state": "Iowa",
+        "county_province": null,
+        "postal_code": "52001",
+        "country": "United States",
+        "longitude": null,
+        "latitude": null,
+        "phone": null,
+        "website_url": "http://www.dimensionalbrewing.com",
+        "updated_at": "2018-08-11T00:00:00.000Z",
+        "created_at": "2018-07-24T00:00:00.000Z"
+      },
+      {
+        "id": "dixie-brewing-co-inc-new-orleans",
+        "name": "Dixie Brewing Co Inc.",
+        "brewery_type": "contract",
+        "street": "6221 S Claiborne Ave Ste 101",
+        "address_2": null,
+        "address_3": null,
+        "city": "New Orleans",
+        "state": "Louisiana",
+        "county_province": null,
+        "postal_code": "70125-4191",
+        "country": "United States",
+        "longitude": null,
+        "latitude": null,
+        "phone": "5048228711",
+        "website_url": null,
+        "updated_at": "2018-08-11T00:00:00.000Z",
+        "created_at": "2018-07-24T00:00:00.000Z"
+      },
+      {
+        "id": "epidemic-ales-concord",
+        "name": "Epidemic Ales",
+        "brewery_type": "micro",
+        "street": "150 Mason Circle Stes I&J",
+        "address_2": null,
+        "address_3": null,
+        "city": "Concord",
+        "state": "California",
+        "county_province": null,
+        "postal_code": "94520",
+        "country": "United States",
+        "longitude": null,
+        "latitude": null,
+        "phone": "9255668850",
+        "website_url": "http://www.epidemicales.com",
+        "updated_at": "2018-08-11T00:00:00.000Z",
+        "created_at": "2018-07-24T00:00:00.000Z"
+      },
+      {
+        "id": "goose-island-philadelphia-philadelphia",
+        "name": "Goose Island Philadelphia",
+        "brewery_type": "brewpub",
+        "street": "1002 Canal St",
+        "address_2": null,
+        "address_3": null,
+        "city": "Philadelphia",
+        "state": "Pennsylvania",
+        "county_province": null,
+        "postal_code": "19123",
+        "country": "United States",
+        "longitude": "-75.13506341",
+        "latitude": "39.9648491",
+        "phone": null,
+        "website_url": null,
+        "updated_at": "2018-08-24T00:00:00.000Z",
+        "created_at": "2018-07-24T00:00:00.000Z"
+      },
+      {
+        "id": "grits-brewing-chamblee",
+        "name": "GRITS Brewing",
+        "brewery_type": "planning",
+        "street": null,
+        "address_2": null,
+        "address_3": null,
+        "city": "Chamblee",
+        "state": "Georgia",
+        "county_province": null,
+        "postal_code": "30341-2024",
+        "country": "United States",
+        "longitude": null,
+        "latitude": null,
+        "phone": null,
+        "website_url": null,
+        "updated_at": "2018-08-11T00:00:00.000Z",
+        "created_at": "2018-07-24T00:00:00.000Z"
+      },
+      {
+        "id": "ironbark-brewery-jackson",
+        "name": "Ironbark Brewery",
+        "brewery_type": "micro",
+        "street": "2610 Kibby Rd",
+        "address_2": null,
+        "address_3": null,
+        "city": "Jackson",
+        "state": "Michigan",
+        "county_province": null,
+        "postal_code": "49203-4908",
+        "country": "United States",
+        "longitude": "-84.43762976",
+        "latitude": "42.2188971",
+        "phone": "5177487988",
+        "website_url": null,
+        "updated_at": "2018-08-24T00:00:00.000Z",
+        "created_at": "2018-07-24T00:00:00.000Z"
+      },
+      {
+        "id": "king-fox-brewery-hialeah",
+        "name": "King Fox Brewery",
+        "brewery_type": "planning",
+        "street": null,
+        "address_2": null,
+        "address_3": null,
+        "city": "Hialeah",
+        "state": "Florida",
+        "county_province": null,
+        "postal_code": "33014-5231",
+        "country": "United States",
+        "longitude": null,
+        "latitude": null,
+        "phone": null,
+        "website_url": null,
+        "updated_at": "2018-08-11T00:00:00.000Z",
+        "created_at": "2018-07-24T00:00:00.000Z"
+      },
+      {
+        "id": "la-funke-brewing-bulverde",
+        "name": "La Funke Brewing",
+        "brewery_type": "planning",
+        "street": null,
+        "address_2": null,
+        "address_3": null,
+        "city": "Bulverde",
+        "state": "Texas",
+        "county_province": null,
+        "postal_code": "78163-4156",
+        "country": "United States",
+        "longitude": null,
+        "latitude": null,
+        "phone": "2104602083",
+        "website_url": "http://www.lafunke.com",
+        "updated_at": "2018-07-24T00:00:00.000Z",
+        "created_at": "2018-07-24T00:00:00.000Z"
+      },
+      {
+        "id": "long-way-brewing-radford",
+        "name": "Long Way Brewing",
+        "brewery_type": "planning",
+        "street": null,
+        "address_2": null,
+        "address_3": null,
+        "city": "Radford",
+        "state": "Virginia",
+        "county_province": null,
+        "postal_code": "24141-1403",
+        "country": "United States",
+        "longitude": null,
+        "latitude": null,
+        "phone": null,
+        "website_url": null,
+        "updated_at": "2018-07-24T00:00:00.000Z",
+        "created_at": "2018-07-24T00:00:00.000Z"
+      },
+      {
+        "id": "mallory-brewing-houston",
+        "name": "Mallory Brewing",
+        "brewery_type": "planning",
+        "street": null,
+        "address_2": null,
+        "address_3": null,
+        "city": "Houston",
+        "state": "Texas",
+        "county_province": null,
+        "postal_code": "77018",
+        "country": "United States",
+        "longitude": null,
+        "latitude": null,
+        "phone": null,
+        "website_url": null,
+        "updated_at": "2018-07-24T00:00:00.000Z",
+        "created_at": "2018-07-24T00:00:00.000Z"
+      },
+      {
+        "id": "lincoln-avenue-brewery-pittsburgh",
+        "name": "Lincoln Avenue Brewery",
+        "brewery_type": "planning",
+        "street": null,
+        "address_2": null,
+        "address_3": null,
+        "city": "Pittsburgh",
+        "state": "Pennsylvania",
+        "county_province": null,
+        "postal_code": "15202",
+        "country": "United States",
+        "longitude": null,
+        "latitude": null,
+        "phone": "4124274078",
+        "website_url": null,
+        "updated_at": "2018-08-11T00:00:00.000Z",
+        "created_at": "2018-07-24T00:00:00.000Z"
+      },
+      {
+        "id": "northwoods-brewing-co-northwood",
+        "name": "Northwoods Brewing Co",
+        "brewery_type": "planning",
+        "street": null,
+        "address_2": null,
+        "address_3": null,
+        "city": "Northwood",
+        "state": "New Hampshire",
+        "county_province": null,
+        "postal_code": "3261",
+        "country": "United States",
+        "longitude": null,
+        "latitude": null,
+        "phone": "2073516983",
+        "website_url": null,
+        "updated_at": "2018-08-11T00:00:00.000Z",
+        "created_at": "2018-07-24T00:00:00.000Z"
+      },
+      {
+        "id": "mobtown-brewing-co-baltimore",
+        "name": "Mobtown Brewing Co",
+        "brewery_type": "planning",
+        "street": null,
+        "address_2": null,
+        "address_3": null,
+        "city": "Baltimore",
+        "state": "Maryland",
+        "county_province": null,
+        "postal_code": "21224-4716",
+        "country": "United States",
+        "longitude": null,
+        "latitude": null,
+        "phone": "4104198312",
+        "website_url": null,
+        "updated_at": "2018-08-11T00:00:00.000Z",
+        "created_at": "2018-07-24T00:00:00.000Z"
+      },
+      {
+        "id": "ska-brewing-durango",
+        "name": "SKA Brewing",
+        "brewery_type": "regional",
+        "street": "225 Girard, Bodo Park",
+        "address_2": null,
+        "address_3": null,
+        "city": "Durango",
+        "state": "Colorado",
+        "county_province": null,
+        "postal_code": "81303-6827",
+        "country": "United States",
+        "longitude": null,
+        "latitude": null,
+        "phone": null,
+        "website_url": null,
+        "updated_at": "2018-08-11T00:00:00.000Z",
+        "created_at": "2018-07-24T00:00:00.000Z"
+      },
+      {
+        "id": "snow-belt-brew-chardon",
+        "name": "Snow Belt Brew",
+        "brewery_type": "micro",
+        "street": "9511 Kile Rd",
+        "address_2": null,
+        "address_3": null,
+        "city": "Chardon",
+        "state": "Ohio",
+        "county_province": null,
+        "postal_code": "44024",
+        "country": "United States",
+        "longitude": null,
+        "latitude": null,
+        "phone": null,
+        "website_url": null,
+        "updated_at": "2018-07-24T00:00:00.000Z",
+        "created_at": "2018-07-24T00:00:00.000Z"
+      },
+      {
+        "id": "mosaic-brewing-lancaster",
+        "name": "Mosaic Brewing",
+        "brewery_type": "planning",
+        "street": null,
+        "address_2": null,
+        "address_3": null,
+        "city": "Lancaster",
+        "state": "Ohio",
+        "county_province": null,
+        "postal_code": "43130-4308",
+        "country": "United States",
+        "longitude": null,
+        "latitude": null,
+        "phone": "6149402500",
+        "website_url": "http://www.double.edge.beer",
+        "updated_at": "2018-07-24T00:00:00.000Z",
+        "created_at": "2018-07-24T00:00:00.000Z"
+      },
+      {
+        "id": "straight-shot-brewing-poquoson",
+        "name": "Straight Shot Brewing",
+        "brewery_type": "planning",
+        "street": null,
+        "address_2": null,
+        "address_3": null,
+        "city": "Poquoson",
+        "state": "Virginia",
+        "county_province": null,
+        "postal_code": "23662-0000",
+        "country": "United States",
+        "longitude": null,
+        "latitude": null,
+        "phone": null,
+        "website_url": null,
+        "updated_at": "2018-07-24T00:00:00.000Z",
+        "created_at": "2018-07-24T00:00:00.000Z"
+      }
+    ];
+
+
+  fetch("https://api.openbrewerydb.org/breweries")
+.then(data => data.json())
+.then(breweries => {
+
+})
+
+
+breweries.forEach((data) => {
+    console.log(data);
+
+  
+
+   breweries.innerHTML += `
+    <div class="col-12 col-sm-6 col-md-3">
+        <div class="breweries">
+        
+    <h3>${data.brewery}</h3>
+            <p><b>Id:</b> ${data.id}</p>
+            <p><b>Name:</b> ${data.name}</p>
+            <p><b>City:</b> ${data.city}</p>
+        </div>
+    </div>
+    `;
+});
+ 
+
+//THOUGH GET METHOD
+
+//$.getJSON("https://api.openbrewerydb.org/breweries", function(data) {
+  //      console.log(data)
+   // }
+//); 
+
+// DIRECTLY 
+
+/*
+
+const apiBreweries = {
+    url : 'https://api.openbrewerydb.org/breweries',
+    
+    id:'10',
+    
+}
+
+const {url, name, id, city} = apiBreweries
+const apiUrl = `${url}${name}/${id}/${city}`
+
+fetch(apiUrl)
+    .then((data) => data.json())
+    .then((breweries) => generateHtml(name))
+
+    const generateHtml =(data) => {
+        console.log(data)
+        consthtml = `
+        <div class="name">${data.name}</div>
+        <div class="detail;">
+        <span>Address: ${data.id}</span>
+        <span>City: ${data.City}</span>
+        </div>
+        `
+
+        const breweriesDiv = document.querySelector('.breweries')
+        breweriesDiv.innerHTML =html
+    }
+
+*/
